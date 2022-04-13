1bcb8281045fefd06741fb892e8d6740bcebba55
Thowhid31 <thowhidantor16@gmail.com>
2022-04-13 22:01:14 +0600

    added environment variables

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -11,7 +11,9 @@ const Register = () => {
 
     const handleRegister = event => {
         event.preventDefault();
-        
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
     }
     return (
         <div className='register-form'>

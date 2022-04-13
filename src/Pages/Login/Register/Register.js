8044dc689b07cf8ba356c5bcebdc93c3815b66d6
Thowhid31 <thowhidantor16@gmail.com>
2022-04-13 21:45:47 +0600

    added reg and login combine

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Register.css'
 
 const Register = () => {
+
+    const navigate = useNavigate()
+    const navigateLogin = () => {
+        navigate (`/login`)
+    }
+
+    const handleRegister = event => {
+        event.preventDefault();
+        
+    }
     return (
-        <div>
-            <h1>Please Register</h1>
-            <form>
-                <input type="text" name='name' id='' />
-                <br/>
-                <input type="email" name='email' id=''/>
-                <br/>
-                <input type="password" name='password' id='' />
+        <div className='register-form'>
+            <h1 style={{textAlign: 'center'}}>Please Register</h1>
+            <form onSubmit={handleRegister}>
+                <input type="text" name='name' id='' placeholder='Your Namee' required/>
+               
+                <input type="email" name='email' id='' placeholder='Email' required/>
+                
+                <input type="password" name='password' id='' placeholder='Your Password' required/>
+                <input type="submit" value="Register" />
             </form>
+            <p>Already Ready with US? <Link to='/login' className='text-danger text-decoration-none' onClick={navigateLogin}>Please Login</Link></p>
         </div>
     );
 };

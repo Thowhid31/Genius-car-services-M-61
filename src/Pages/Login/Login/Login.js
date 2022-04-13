5b53a12bcb6878d4d263cc3ae7a5a2d44b2cc272
Thowhid31 <thowhidantor16@gmail.com>
2022-04-13 21:33:32 +0600

    added register part

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,9 +1,11 @@
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Login = () => {
     const emailRef = useRef('');
-    const passRef = useRef('')
+    const passRef = useRef('');
+    const navigate = useNavigate()
 
     const formHandleSubmit = event => {
         event.preventDefault();
@@ -13,13 +15,17 @@ const Login = () => {
         console.log(password);
     }
 
+    const navigateRegister = event => {
+        navigate('/register')
+    }
+
     return (
         <div className='container w-50 mx-auto'>
             <h3 className='mt-3 text-center text-primary'>Please Login</h3>
             <Form onSubmit={formHandleSubmit}>
   <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label>Email address</Form.Label>
-    <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
+    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required/>
     <Form.Text className="text-muted">
       We'll never share your email with anyone else.
     </Form.Text>
@@ -27,7 +33,7 @@ const Login = () => {
 
   <Form.Group className="mb-3" controlId="formBasicPassword">
     <Form.Label>Password</Form.Label>
-    <Form.Control ref={passRef} type="password" placeholder="Password" />
+    <Form.Control ref={passRef} type="password" placeholder="Password" required/>
   </Form.Group>
   <Form.Group className="mb-3" controlId="formBasicCheckbox">
     <Form.Check type="checkbox" label="Check me out" />
@@ -36,6 +42,7 @@ const Login = () => {
     Submit
   </Button>
 </Form>
+<p>New to Genius Car? <Link to='/register' className='text-danger text-decoration-none' onClick={navigateRegister}>Please Register</Link></p>
         </div>
     );
 };
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -4,6 +4,13 @@ const Register = () => {
     return (
         <div>
             <h1>Please Register</h1>
+            <form>
+                <input type="text" name='name' id='' />
+                <br/>
+                <input type="email" name='email' id=''/>
+                <br/>
+                <input type="password" name='password' id='' />
+            </form>
         </div>
     );
 };
